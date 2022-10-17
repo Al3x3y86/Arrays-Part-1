415c92e5dff6152e3f9fb137b159473644783807
Alexey <alexey_kuzminykh@mail.ru>
2022-10-17 23:50:37 +0300

    HW 3.4 Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] weight = {1, 2, 3};
-        double[] temp = {1.57, 7.654, 9.986};
-        char[] speed ={'a', 'b', 'c'};
 
-        for (int i = weight.length - 1; i >= 0 ; i--) {
-            System.out.print(weight[i]);
-            if (i != 0){
-                System.out.print(", ");
+        for (int i = 0; i < weight.length; i++) {
+            if (weight[i] % 2 != 0){
+                weight[i] += 1;
             }
         }
-        System.out.println();
+        System.out.println(Arrays.toString(weight));
 
-        for (int i = temp.length - 1; i >= 0; i--) {
-            System.out.print(temp[i]);
-            if (i != 0){
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-
-        for (int i = speed.length - 1; i >= 0; i--) {
-            System.out.print(speed[i]);
-            if (i != 0){
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
     }
 }

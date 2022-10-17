4928b6769ba702470edb0a6095a8d11c4c9402f6
Alexey <alexey_kuzminykh@mail.ru>
2022-10-17 23:17:28 +0300

    HW 3.1 Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] weight = new int [3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+
+        double[] temp = {1.57, 7.654, 9.986};
+
+        char[] speed ={'a', 'b', 'c'};
     }
 }

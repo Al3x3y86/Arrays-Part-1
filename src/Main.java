3e6a8f47e6c5b51d43a35a50163b49aa9f97cd41
Alexey <alexey_kuzminykh@mail.ru>
2022-10-17 23:34:52 +0300

    HW 3.3 Задача 3

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,25 @@ public class Main {
         double[] temp = {1.57, 7.654, 9.986};
         char[] speed ={'a', 'b', 'c'};
 
-        for (int i = 0; i < weight.length; i++) {
+        for (int i = weight.length - 1; i >= 0 ; i--) {
             System.out.print(weight[i]);
-            if (i != weight.length - 1){
+            if (i != 0){
                 System.out.print(", ");
             }
         }
         System.out.println();
 
-        for (int i = 0; i < temp.length ; i++) {
+        for (int i = temp.length - 1; i >= 0; i--) {
             System.out.print(temp[i]);
-            if (i != temp.length - 1){
+            if (i != 0){
                 System.out.print(", ");
             }
         }
         System.out.println();
 
-        for (int i = 0; i < speed.length ; i++) {
+        for (int i = speed.length - 1; i >= 0; i--) {
             System.out.print(speed[i]);
-            if (i != speed.length - 1){
+            if (i != 0){
                 System.out.print(", ");
             }
         }
